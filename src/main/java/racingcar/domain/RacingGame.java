be9d32e5e33b2f01aa6b2b7e3f9b95ea70827d38
sm4640 <s1m0611@naver.com>
2025-10-27 13:30:04 +0900

    feat(domain): create racingGame class with carList, playRound, printRound, getwinners

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+    private List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
+
+    public void printCarMoveDistance() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+        }
+    }
+
+    public List<Car> getWinners(){
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
+
+
+}
