bf52f6acef5dbae4a71a18c75fd5ddcca0bf65f7
sm4640 <s1m0611@naver.com>
2025-10-27 13:37:44 +0900

    test(domain): create test about racingGame class

---
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+
+    @Test
+    @DisplayName("가장 멀리간 사람이 우승자이며, 동점자까지 우승자")
+    void getWinnerCorrect() {
+        List<Car> cars =List.of(
+                new Car("pobi", 3),
+                new Car("tobi", 5),
+                new Car("tubi", 5)
+        );
+
+        RacingGame game = new RacingGame(cars);
+        List<Car> winners = game.getWinners();
+
+        assertThat(winners).extracting("name")
+                .containsExactlyInAnyOrder("tobi", "tubi");
+    }
+
+}
